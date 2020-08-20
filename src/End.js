afa8a2ad8881a3c81d4e9db3838b16db4871061a
fateshub <realbearinc@gmail.com>
2020-08-19 20:08:33 -0700

    Update End.js

--- src/End.js
+++ src/End.js
@@ -10,24 +10,19 @@ const End = ({correctLetters, wrongLetters, selectedWord, setPlayable}) => {
     if( selectedWord.length >0 && checkWin(correctLetters, wrongLetters, selectedWord) === 'win' ) {
       finalMessage = 'Chat is 5Head';
       playable = false;
-      setTimeout(function(){  document.getElementsByClassName("popup-container").style.display = "none";}, 3000);
-
     } else if( checkWin(correctLetters, wrongLetters, selectedWord) === 'lose' ) {
       finalMessage = 'Chat Sucks lol';
       finalMessageRevealWord = `the word was: ${selectedWord}`;
       playable = false;
-      setTimeout(function(){       document.getElementsByClassName("popup-container").style.display = "none";
-    }, 3000);
     }
     else{
         playable = true;
-        document.getElementsByClassName("popup-container").style.display = "block";
     }
   
     useEffect(() => {
       setPlayable(playable);
     });
-    
+     
     return (
       <div className="popup-container" style={finalMessage !== '' ? {display:'flex'} : {}}>
         <div className="popup">
